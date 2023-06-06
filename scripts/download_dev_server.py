992ff4d2468450c012e9466a43f0ad20ad8534b7
estebangarcia21 <estebangarcia2121@gmail.com>
2023-06-05 20:11:40 -0700

    Add dowload_dev_server.py script

--- scripts/download_dev_server.py
+++ scripts/download_dev_server.py
@@ -1,1 +1,42 @@
-print('UNIMPLEMENTED')
+import os
+import shutil
+
+import boto3
+
+PROJECT_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+
+def download_s3_folder(bucket_name, s3_folder, local_dir=None):
+    s3 = boto3.resource("s3")
+
+    """
+    Download the contents of a folder directory
+    Args:
+        bucket_name: the name of the s3 bucket
+        s3_folder: the folder path in the s3 bucket
+        local_dir: a relative or absolute directory path in the local file system
+    """
+    bucket = s3.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        target = obj.key if local_dir is None \
+            else os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
+        if not os.path.exists(os.path.dirname(target)):
+            os.makedirs(os.path.dirname(target))
+        if obj.key[-1] == '/':
+            continue
+        bucket.download_file(obj.key, target)
+
+
+bucket_name = 'the-pit-community'
+folder_prefix = '.local-server/'
+destination_path = os.path.join(PROJECT_ROOT_DIR, '.local-server')
+
+if os.path.exists(destination_path):
+    shutil.rmtree(destination_path)
+    os.mkdir(destination_path)
+else:
+    os.mkdir(destination_path)
+
+print('Cloning development server...')
+download_s3_folder(bucket_name, folder_prefix, destination_path)
+print('Done!')
