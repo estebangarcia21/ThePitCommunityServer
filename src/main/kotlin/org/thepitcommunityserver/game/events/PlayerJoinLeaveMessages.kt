beb6d5b555bd85362c4d7628b7434d887a596904
Haberno <Deathersmc@gmail.com>
2023-06-06 12:11:18 -0400

    Remove leave message

--- src/main/kotlin/org/thepitcommunityserver/game/events/PlayerJoinLeaveMessages.kt
+++ src/main/kotlin/org/thepitcommunityserver/game/events/PlayerJoinLeaveMessages.kt
@@ -14,11 +14,4 @@ object PlayerJoinLeaveMessages : Listener {
             (ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "Welcome Back to The Pit Community Server! " + ChatColor.AQUA
                     + event.player.name + ChatColor.GREEN + ", thank you for coming back ${ChatColor.RED.toString() + Text.HEART}")
     }
-
-    @EventHandler
-    fun onLeave(event: PlayerQuitEvent) {
-        event.quitMessage =
-            (ChatColor.RED.toString() + ChatColor.BOLD.toString() + "Goodbye! " + ChatColor.AQUA
-                    + event.player.name + ChatColor.RED + " just left the server!")
-    }
 }
