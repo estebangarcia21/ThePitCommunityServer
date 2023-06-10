42121549a21fff94bdd3cbebd9a00b17a77ec2ba
Haberno <Deathersmc@gmail.com>
2023-06-10 00:20:19 -0400

    Add bullet time and crush

--- src/main/kotlin/org/thepitcommunityserver/util/Text.kt
+++ src/main/kotlin/org/thepitcommunityserver/util/Text.kt
@@ -9,8 +9,8 @@ fun formatPercentage(percent: Double?): String {
     return "${((percent ?: 0.0) * 100).toInt()}%"
 }
 
-fun intToRoman(num: Int): String {
-    require(num in 1..5) { "Input number must be between 1 and 5 (inclusive)." }
+fun intToRoman(num: Int?): String {
+    require(num in 1..10) { "Input number must be between 1 and 5 (inclusive)." }
 
     val romanNumerals = mapOf(
         1 to "I",
