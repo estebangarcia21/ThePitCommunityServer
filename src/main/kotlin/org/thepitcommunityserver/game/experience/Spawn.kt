21fb2d03d163a09e2c5ba253d74189c76a8041da
estebangarcia21 <estebangarcia2121@gmail.com>
2023-06-10 18:24:09 -0700

    Set bullet time priority to high

--- src/main/kotlin/org/thepitcommunityserver/game/experience/Spawn.kt
+++ src/main/kotlin/org/thepitcommunityserver/game/experience/Spawn.kt
@@ -1,16 +1,25 @@
 package org.thepitcommunityserver.game.experience
 
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerRespawnEvent
 import org.thepitcommunityserver.util.listToLocation
 import org.thepitcommunityserver.util.getMapData
 
 object Spawn : Listener {
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) {
-        val player = event.player
+        handleSpawn(event.player)
+    }
 
+    @EventHandler
+    fun onRespawn(event: PlayerRespawnEvent) {
+        handleSpawn(event.player)
+    }
+
+    private fun handleSpawn(player: Player) {
         val mapData = getMapData()
         val spawnLoc = mapData.elemental.spawnPoints.random()
 
