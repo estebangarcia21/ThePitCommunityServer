d79e71135902711eb35bc2557d13a50a94140cd2
Haberno <Deathersmc@gmail.com>
2023-06-16 22:11:18 -0400

    Add knockback

---
+++ src/main/kotlin/org/thepitcommunityserver/game/enchants/Knockback.kt
@@ -0,0 +1,38 @@
+package org.thepitcommunityserver.game.enchants
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.util.Vector
+import org.thepitcommunityserver.game.enchants.lib.*
+import org.thepitcommunityserver.util.intToRoman
+import org.thepitcommunityserver.util.undefPropErr
+
+object Knockback : Enchant {
+    override val config: EnchantConfig
+        get() = EnchantConfig(
+            name = "Knockback",
+            tiers = listOf(1, 2, 3),
+            group = EnchantGroup.B,
+            rare = true,
+            type = EnchantType.SWORD
+        ) { "Increases knockback taken by<br/>enemies by <white>${knockbackAmount[it]} blocks</white>" }
+
+    private val knockbackAmount = mapOf(
+        1 to 3,
+        2 to 4,
+        3 to 6
+    )
+
+    @EventHandler
+    fun onDamageEvent(event: EntityDamageByEntityEvent) {
+        event.damagerMeleeHitPlayerWithEnchant(this){_, damaged, tier, _, ->
+            val knockbackAmount = knockbackAmount[tier] ?: undefPropErr("knockbackAmount", tier)
+
+            damaged.velocity = Vector(
+                damaged.velocity.x * knockbackAmount,
+                damaged.velocity.y * knockbackAmount,
+                damaged.velocity.z * knockbackAmount
+            )
+        }
+    }
+}
