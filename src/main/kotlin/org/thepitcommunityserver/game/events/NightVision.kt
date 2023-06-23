9c27262047c7262e135b9d94377563862644ef48
Haberno <52333828+Haberno@users.noreply.github.com>
2023-06-23 03:02:38 -0400

    Give night vision

---
+++ src/main/kotlin/org/thepitcommunityserver/game/events/NightVision.kt
@@ -0,0 +1,17 @@
+package org.thepitcommunityserver.game.events
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+
+object NightVision : Listener {
+
+    @EventHandler
+    fun onJoin(event: PlayerJoinEvent) {
+        val player = event.player
+
+        player.addPotionEffect(PotionEffect(PotionEffectType.NIGHT_VISION, 1, Int.MAX_VALUE, true))
+    }
+}
