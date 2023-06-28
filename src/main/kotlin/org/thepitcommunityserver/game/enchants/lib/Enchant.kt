c5eebaf4fa05df36e8f15a386adbd858819c8644
estebangarcia21 <estebangarcia2121@gmail.com>
2023-06-27 22:11:40 -0700

    Add ArmorChange

--- src/main/kotlin/org/thepitcommunityserver/game/enchants/lib/Enchant.kt
+++ src/main/kotlin/org/thepitcommunityserver/game/enchants/lib/Enchant.kt
@@ -21,7 +21,8 @@ val Enchants = listOf(
     MegaLongbow,
     Assassin,
     Peroxide,
-    SpeedyHit
+    SpeedyHit,
+    SlimePack
 )
 
 interface Enchant : Listener {
@@ -58,10 +59,11 @@ fun enchantByName(name: String): Enchant? {
 /**
  * Gets enchantments in `Name -> Level` manner.
  */
-fun getItemMysticEnchantments(item: ItemStack?): Map<String, Int> {
+fun getItemMysticEnchantments(item: ItemStack?): Map<String, Int>? {
     if (item == null) return emptyMap()
 
-    val nmsItemStack = CraftItemStack.asNMSCopy(item)
+    val nmsItemStack = CraftItemStack.asNMSCopy(item) ?: return null
+
     val compound = nmsItemStack.tag ?: return emptyMap()
 
     val enchantments = mutableMapOf<String, Int>()
@@ -101,5 +103,5 @@ fun setItemMysticEnchantments(item: ItemStack?, enchantments: Map<String, Int>) 
 fun getEnchantTierForItem(enchant: Enchant, item: ItemStack?): Int? {
     if (item == null) return null
 
-    return getItemMysticEnchantments(item)[enchant.config.name]
+    return getItemMysticEnchantments(item)?.get(enchant.config.name)
 }
