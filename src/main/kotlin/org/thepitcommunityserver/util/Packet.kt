f3054df6f5fea982e52f1cfdfc8a7423660be417
Haberno <52333828+Haberno@users.noreply.github.com>
2023-06-28 02:57:21 -0400

    Add combo stun

--- src/main/kotlin/org/thepitcommunityserver/util/Packet.kt
+++ src/main/kotlin/org/thepitcommunityserver/util/Packet.kt
@@ -1,15 +1,18 @@
 package org.thepitcommunityserver.util
 
-import net.minecraft.server.v1_8_R3.IChatBaseComponent
-import net.minecraft.server.v1_8_R3.Packet
-import net.minecraft.server.v1_8_R3.PacketListener
-import net.minecraft.server.v1_8_R3.PacketPlayOutChat
+import net.minecraft.server.v1_8_R3.*
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
 import org.bukkit.entity.Player
 
-fun createChatPacket(text: String): PacketPlayOutChat {
+
+fun createChatComponent(text: String): IChatBaseComponent? {
     val message = "{\"text\":\"$text\"}"
-    val serializer = IChatBaseComponent.ChatSerializer.a(message)
+
+    return IChatBaseComponent.ChatSerializer.a(message)
+}
+
+fun createChatPacket(message: String): PacketPlayOutChat {
+    val serializer = createChatComponent(message)
     val position: Byte = 2
 
     return PacketPlayOutChat(serializer, position)
