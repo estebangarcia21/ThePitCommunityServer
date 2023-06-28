029154c85f5f6308193fb1de5fe7fa091c6ca3bf
Haberno <52333828+Haberno@users.noreply.github.com>
2023-06-27 23:41:00 -0400

    Add armor damage function

--- src/main/kotlin/org/thepitcommunityserver/game/enchants/Perun.kt
+++ src/main/kotlin/org/thepitcommunityserver/game/enchants/Perun.kt
@@ -1,8 +1,10 @@
 package org.thepitcommunityserver.game.enchants
 
+import net.minecraft.server.v1_8_R3.ItemArmor
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.inventory.ItemStack
 import org.thepitcommunityserver.game.enchants.lib.*
 import org.thepitcommunityserver.game.events.DamageManager
 import org.thepitcommunityserver.util.HitCounter
@@ -35,7 +37,7 @@ object Perun : Enchant {
         3 to 4
     )
 
-    val hearts = damageAmount.mapValues { it.value / 2f}
+    val hearts = damageAmount.mapValues { it.value / 2f }
 
     private val description: EnchantDescription = {
         val lastMessage = ChatColor.ITALIC.toString() + "Lightning deals true damage"
@@ -47,8 +49,7 @@ object Perun : Enchant {
         )[it]
 
         if (it == 3) {
-            "Every <yellow>$word</yellow> hit strikes<br/><yellow>lightning</yellow> for <red>${hearts[it]}❤</red> + " +
-                    "<red>1❤</red><br/>per <aqua>diamond piece</aqua> on your<br/>victim.<br/>$lastMessage"
+            "Every <yellow>$word</yellow> hit strikes<br/><yellow>lightning</yellow> for <red>${hearts[it]}❤</red> + <red>1❤</red><br/>per <aqua>diamond piece</aqua> on your<br/>victim.<br/>$lastMessage"
         } else {
             "Every <yellow>$word</yellow> hit strikes<br/><yellow>lightning</yellow> for <red>${hearts[it]}❤</red><br/>$lastMessage"
         }
@@ -56,7 +57,7 @@ object Perun : Enchant {
 
     @EventHandler
     fun onDamageEvent(event: EntityDamageByEntityEvent) {
-        event.damagerMeleeHitPlayerWithEnchant(this){
+        event.damagerMeleeHitPlayerWithEnchant(this) {
             val damager = it.damager
             val damaged = it.damaged
 
@@ -65,10 +66,9 @@ object Perun : Enchant {
 
             hitCounter.onNthHit(damager.uniqueId, hitsNeeded) {
                 if (it.enchantTier == 3) {
-                    damaged.inventory.armorContents.forEach{ a ->
-                        if (a == null) return@forEach
+                    damaged.inventory.armorContents.filterNotNull().forEach{ a ->
 
-                        damageAmount += if (a.type.name.split("_")[0].equals("diamond", ignoreCase = true)) 2 else 0
+                        damageAmount += addArmorDamage(a)
                     }
                 }
 
@@ -76,5 +76,12 @@ object Perun : Enchant {
                 DamageManager.applyTrueDamage(damaged, damager, damageAmount)
             }
         }
+    }
+    private fun addArmorDamage(armor: ItemStack): Double {
+        val name = armor.type.name.split("_")[0]
+
+        if (!name.equals("diamond", ignoreCase = true)) return 0.0
+
+        return 2.0
     }
 }
