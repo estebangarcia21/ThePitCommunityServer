9a1ea781264d35a0f12488761cf861c62b5aa4b6
Haberno <Deathersmc@gmail.com>
2023-06-28 15:44:02 -0400

    Add feature to super monkey

--- src/main/kotlin/org/thepitcommunityserver/game/enchants/SuperMonkey.kt
+++ src/main/kotlin/org/thepitcommunityserver/game/enchants/SuperMonkey.kt
@@ -2,8 +2,12 @@ package org.thepitcommunityserver.game.enchants
 
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import org.bukkit.util.Vector
 import org.thepitcommunityserver.game.enchants.lib.*
+import org.thepitcommunityserver.util.SECONDS
+import org.thepitcommunityserver.util.Time
 import org.thepitcommunityserver.util.leggings
 
 object SuperMonkey : Enchant {
@@ -14,8 +18,10 @@ object SuperMonkey : Enchant {
             group = EnchantGroup.B,
             rare = true,
             type = EnchantType.PANTS
-        ) { "Climb like <yellow>in4red</yellow>" }
+        ) { "Climb like <yellow>in4red</yellow>. Gives <green>Jump Boost II</green> and <yellow>Speed II</yellow> 30s." }
 
+
+    private val duration = Time(30L * SECONDS)
     @EventHandler
     fun onMove(event: PlayerMoveEvent) {
         val player = event.player
@@ -29,5 +35,9 @@ object SuperMonkey : Enchant {
             // Climb like a monkey!!!
             player.velocity = Vector(0.0, 0.5, 0.0)
         }
+
+        player.addPotionEffect(PotionEffect(PotionEffectType.JUMP, duration.ticks().toInt(), 1, true))
+        player.addPotionEffect(PotionEffect(PotionEffectType.SPEED, duration.ticks().toInt(), 1, true))
+
     }
 }
