5f7ea2e005622707b61389c1435f50d6d879a4bd
Haberno <52333828+Haberno@users.noreply.github.com>
2023-07-01 01:42:51 -0400

    Add healer (#7)
    * Add healer
    
    * Update timer
    
    * Update legacy code
    
    ---------
    
    Co-authored-by: Esteban Garcia <44991472+estebangarcia21@users.noreply.github.com>

---
+++ src/main/kotlin/org/thepitcommunityserver/game/enchants/Healer.kt
@@ -0,0 +1,45 @@
+package org.thepitcommunityserver.game.enchants
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.thepitcommunityserver.game.enchants.lib.*
+import org.thepitcommunityserver.game.events.DamageManager
+import org.thepitcommunityserver.util.*
+import java.util.UUID
+
+object Healer : Enchant {
+    override val config: EnchantConfig
+        get() = EnchantConfig(
+            name = "Healer",
+            tiers = listOf(1, 2, 3),
+            group = EnchantGroup.B,
+            rare = true,
+            type = EnchantType.SWORD,
+        ) { "Hitting a player <green>heals</green> both you and them for <red>${hearts[it]}${Text.HEART}</red> (1s cooldown)" }
+
+    private val healAmount = mapOf(
+        1 to 2.0,
+        2 to 4.0,
+        3 to 6.0
+    )
+
+    private val hearts = healAmount.mapValues { it.value / 2f }
+
+    private val timer = Timer<UUID>()
+    private val cooldownTime = Time(1L * SECONDS)
+
+    @EventHandler
+    fun onDamageEvent(event: EntityDamageByEntityEvent) {
+        event.damagerMeleeHitPlayerWithEnchant(this) {
+            val damager = it.damager
+            val damaged = it.damaged
+
+            val healAmount = healAmount[it.enchantTier] ?: undefPropErr("healAmount", it.enchantTier)
+
+            timer.cooldown(damager.uniqueId, cooldownTime.ticks()) {
+                DamageManager.applyHeal(damager, healAmount)
+                DamageManager.applyHeal(damaged, healAmount)
+            }
+        }
+    }
+}
