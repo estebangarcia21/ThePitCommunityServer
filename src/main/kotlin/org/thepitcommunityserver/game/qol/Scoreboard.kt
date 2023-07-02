e8a0a35f2565417041248f721ac6648ae3e6474b
Esteban Garcia <44991472+estebangarcia21@users.noreply.github.com>
2023-07-01 20:08:42 -0700

    Add Flickerless scoreboard implementation and GUI builder helpers (#38)
    * Add build item
    
    * Add Flickerless scoreboard implementation and GUI builder helpers

--- src/main/kotlin/org/thepitcommunityserver/game/qol/Scoreboard.kt
+++ src/main/kotlin/org/thepitcommunityserver/game/qol/Scoreboard.kt
@@ -6,8 +6,11 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.scoreboard.DisplaySlot
+import org.bukkit.scoreboard.Objective
+import org.bukkit.scoreboard.Scoreboard
+import org.bukkit.scoreboard.Team
 import org.thepitcommunityserver.db.data
-import org.thepitcommunityserver.external.NativeScoreboard
 import org.thepitcommunityserver.game.combat.CombatStatus
 import org.thepitcommunityserver.util.*
 import java.text.DecimalFormat
@@ -15,6 +18,8 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object PitScoreboard : Listener {
+    private val scoreboards = mutableMapOf<Player, FlickerlessScoreboard>()
+
     init {
         GlobalTimer.registerTask("scoreboard-updater", 1 * SECONDS) {
             CurrentWorld.players.forEach(::renderScoreboardView)
@@ -25,23 +30,29 @@ object PitScoreboard : Listener {
     fun onPlayerJoin(event: PlayerJoinEvent) = renderScoreboardView(event.player)
 
     private fun renderScoreboardView(player: Player) {
-        val scoreboard = Bukkit.getScoreboardManager().newScoreboard
-        val optimizedBoard = NativeScoreboard(scoreboard)
+        val nativeBoard = scoreboards.getOrPut(player) { FlickerlessScoreboard(Bukkit.getScoreboardManager().newScoreboard) }
+
         val simpleDateFormat = SimpleDateFormat("MM/dd/yy")
         val date = Date()
         val decimalFormat = DecimalFormat("#0.00")
+
+        player.data.apply {
+            gold += 10
+        }
         val playerData = player.data
 
-        optimizedBoard.title(ChatColor.YELLOW.toString() + ChatColor.BOLD + "THE BLUE HATS PIT")
-        optimizedBoard.line(appendColors(ChatColor.GRAY.toString() + simpleDateFormat.format(date) + " " + ChatColor.DARK_GRAY + "mega69L"))
-        optimizedBoard.line(" ")
+        nativeBoard.update()
+
+        nativeBoard.title(ChatColor.YELLOW.toString() + ChatColor.BOLD + "THE BLUE HATS PIT")
+        nativeBoard.line(appendColors(ChatColor.GRAY.toString() + simpleDateFormat.format(date) + " " + ChatColor.DARK_GRAY + "mega69L"))
+        nativeBoard.line(" ")
 
         if (player.data.prestige > 0) {
-            optimizedBoard.line(formatLine("Prestige", ChatColor.YELLOW, intToRoman(playerData.prestige), false))
+            nativeBoard.line(formatLine("Prestige", ChatColor.YELLOW, intToRoman(playerData.prestige), false))
         }
 
-        optimizedBoard.line(formatLine("Level", null, formatLevel(playerData.level, playerData.prestige), false))
-        optimizedBoard.line(
+        nativeBoard.line(formatLine("Level", null, formatLevel(playerData.level, playerData.prestige), false))
+        nativeBoard.line(
             formatLine(
                 "Needed XP",
                 ChatColor.AQUA,
@@ -49,8 +60,8 @@ object PitScoreboard : Listener {
                 false
             )
         )
-        optimizedBoard.line("  ")
-        optimizedBoard.line(
+        nativeBoard.line("  ")
+        nativeBoard.line(
             formatLine(
                 "Gold",
                 ChatColor.GOLD,
@@ -58,12 +69,12 @@ object PitScoreboard : Listener {
                 false
             )
         )
-        optimizedBoard.line("   ")
-        optimizedBoard.line(formatLine("Status", null, formatStatus(player), false))
-        optimizedBoard.line("    ")
-        optimizedBoard.line(appendColors(ChatColor.YELLOW.toString() + "play.thebluehatspit.net"))
+        nativeBoard.line("   ")
+        nativeBoard.line(formatLine("Status", null, formatStatus(player), false))
+        nativeBoard.line("    ")
+        nativeBoard.line(appendColors(ChatColor.YELLOW.toString() + "play.thebluehatspit.net"))
 
-        player.scoreboard = scoreboard
+        player.scoreboard = nativeBoard.scoreboard
     }
 
     private fun formatLevel(level: Int, prestigeLevel: Int): String {
@@ -129,5 +140,95 @@ object PitScoreboard : Listener {
 //        return if (combatStatus === CombatStatus.COMBAT) formattedStatus + " " + ChatColor.RESET + ChatColor.GRAY + "(" + combatManager.getCombatTime(
 //            player
 //        ) + ")" else formattedStatus
+    }
+}
+
+private const val MAX_LINES = 15
+private const val MAX_CHARS = 16
+
+class FlickerlessScoreboard(val scoreboard: Scoreboard) {
+
+    private val objective: Objective
+    private val teams = mutableMapOf<Int, Team>()
+
+    private var updateIndex = 0
+
+    fun update() {
+        updateIndex = 0
+    }
+
+    fun title(title: String) {
+        objective.displayName = title
+    }
+
+    fun line(text: String) {
+        val team = teams[updateIndex] ?: return
+
+        val (prefix, suffix) = splitStringInMiddle(segmentColors(text))
+        team.prefix = prefix
+        team.suffix = suffix
+
+        objective.getScore(getScoreIndex(updateIndex)).score = MAX_LINES - updateIndex
+        updateIndex++
+    }
+
+    private fun getScoreIndex(index: Int): String {
+        return String(charArrayOf(ChatColor.COLOR_CHAR, ('s'.toInt() + index).toChar()))
+    }
+
+    init {
+        scoreboard.clearSlot(DisplaySlot.SIDEBAR)
+        objective = scoreboard.registerNewObjective("sidebar", "dummy")
+        objective.displaySlot = DisplaySlot.SIDEBAR
+
+        val initialTeams = arrayOfNulls<String>(MAX_LINES)
+        initialTeams.forEachIndexed { i, _ ->
+            val scoreIndex = getScoreIndex(i)
+            scoreboard.registerNewTeam(scoreIndex).also { teams[i] = it }.addEntry(scoreIndex)
+        }
+    }
+
+    private fun segmentColors(string: String): String {
+        if (string.length >= 16) {
+            val base = string.substring(0, 16)
+            val end = string.substring(16)
+            val colors = ArrayList<String>()
+
+            run loop@ {
+                (base.length - 1 downTo 0).forEach { i ->
+                    if (base[i] == ChatColor.COLOR_CHAR) {
+                        colors.add(base.substring(i, i + 2))
+
+                        val charCheck = i - 2
+
+                        if (charCheck < 0) return@forEach
+
+                        if (base[charCheck] != ChatColor.COLOR_CHAR) return@loop
+                    }
+                }
+            }
+
+            val modifiers = StringBuilder()
+            val colorsSize = colors.size
+
+            (0 until colorsSize - 1).forEach { i ->
+                modifiers.append(colors[i])
+            }
+
+            return base + colors[colorsSize - 1] + modifiers + end
+        }
+
+        return string
+    }
+}
+
+fun splitStringInMiddle(input: String): Pair<String, String> {
+    val maxLength = MAX_CHARS
+    return if (input.length > maxLength) {
+        val firstHalf = input.substring(0, maxLength)
+        val secondHalf = input.substring(maxLength)
+        Pair(firstHalf, secondHalf)
+    } else {
+        Pair(input, "")
     }
 }
--- src/main/kotlin/org/thepitcommunityserver/util/NPC.kt
+++ src/main/kotlin/org/thepitcommunityserver/util/NPC.kt
@@ -10,7 +10,9 @@ import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
+import org.thepitcommunityserver.db.data
 import org.thepitcommunityserver.registerEvents
 
 private val CITIZENS_REGISTRY = CitizensAPI.getNPCRegistry()
@@ -26,13 +28,89 @@ val worldNPCS = listOf(
         gui = GUI(
             title = "Non-permanent items",
             rows = 3,
-            contents = mapOf(
-                11 to ItemStack(Material.DIAMOND_SWORD),
-                12 to ItemStack(Material.OBSIDIAN, 8),
-                13 to ItemStack(Material.GOLD_PICKAXE),
-                14 to ItemStack(Material.DIAMOND_CHESTPLATE),
-                15 to ItemStack(Material.DIAMOND_BOOTS)
-            ),
+            onOpen = { player ->
+                val gold = player.data.gold
+
+                fun purchaseableLore(price: Double, vararg lore: String): List<String> {
+                    val purchaseMessage =
+                        if (gold >= price) "<yellow>Click to purchase!</yellow>" else "<red>Not enough gold!</red>"
+
+                    return buildLore(
+                        *lore,
+                        "",
+                        "<italic>Lost on death</italic>",
+                        "Cost: <gold>${price.toInt()}g</gold>",
+                        purchaseMessage,
+                        defaultColor = ChatColor.GRAY
+                    )
+                }
+
+                fun purchaseableName(price: Double, name: String): String {
+                    val nameColor = if (gold >= price) "yellow" else "red"
+
+                    return "<$nameColor>$name</$nameColor>".parseChatColors()
+                }
+
+                val diamondSwordPrice = 150.0
+                val obsidianPrice = 40.0
+                val goldenPickaxePrice = 40.0
+                val diamondChestplatePrice = 500.0
+                val diamondBootsPrice = 300.0
+
+                setContents(
+                    mapOf(
+                        11 to buildItem(
+                            name = purchaseableName(diamondSwordPrice, "Diamond Sword"),
+                            material = Material.DIAMOND_SWORD,
+                            itemID = "diamond-sword",
+                            lore = purchaseableLore(
+                                diamondSwordPrice,
+                                "<blue>+20% damage vs bountied"
+                            ),
+                            flags = listOf(ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ATTRIBUTES)
+                        ),
+                        12 to buildItem(
+                            name = purchaseableName(obsidianPrice, "Obsidian"),
+                            material = Material.OBSIDIAN,
+                            itemID = "obsidian",
+                            lore = purchaseableLore(
+                                obsidianPrice,
+                                "Remains for 120 seconds"
+                            ),
+                            count = 8
+                        ),
+                        13 to buildItem(
+                            name = purchaseableName(goldenPickaxePrice, "Gold Pickaxe"),
+                            material = Material.GOLD_PICKAXE,
+                            itemID = "gold-pickaxe",
+                            lore = purchaseableLore(
+                                goldenPickaxePrice,
+                                "Breaks a 5-high pillar of",
+                                "obsidian when 2-tapping it"
+                            ),
+                            flags = listOf(ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ATTRIBUTES)
+                        ),
+                        14 to buildItem(
+                            name = purchaseableName(diamondChestplatePrice, "Diamond Chestplate"),
+                            material = Material.DIAMOND_CHESTPLATE,
+                            itemID = "diamond-chestplate",
+                            lore = purchaseableLore(
+                                diamondChestplatePrice,
+                                "Auto-equips on buy!"
+                            )
+                        ),
+                        15 to buildItem(
+                            name = purchaseableName(diamondBootsPrice, "Diamond Boots"),
+                            material = Material.DIAMOND_BOOTS,
+                            itemID = "diamond-boots",
+                            lore = purchaseableLore(
+                                diamondBootsPrice,
+                                "Auto-equips on buy!"
+                            )
+                        )
+                    )
+                )
+            },
             readOnly = true
         ),
         nameHeight = 1.9,
