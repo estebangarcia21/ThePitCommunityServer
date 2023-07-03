b79ad634811a34875b118c8d0f7b5eeedae58b86
Haberno <Deathersmc@gmail.com>
2023-07-02 23:24:54 -0400

    Add player death message

---
+++ src/main/kotlin/org/thepitcommunityserver/game/playerExperience/PlayerDeathMessage.kt
@@ -0,0 +1,23 @@
+package org.thepitcommunityserver.game.playerExperience
+
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.thepitcommunityserver.registerEvents
+import org.thepitcommunityserver.util.createChatComponent
+import org.thepitcommunityserver.util.replaceChatColorTags
+import org.thepitcommunityserver.util.sendPacketToPlayer
+
+object PlayerDeathMessage : Listener {
+    @EventHandler
+    fun playerDeathMessage(event: PlayerDeathEvent) {
+        val player = event.entity ?: return
+
+        val titleMessage = replaceChatColorTags("<red>YOU DIED!</red>")
+        val title = createChatComponent(titleMessage)
+        val titlePacket = PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, title, 20, 60, 60)
+
+        sendPacketToPlayer(player, titlePacket)
+    }
+}
