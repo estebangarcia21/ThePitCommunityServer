a99d303a99182a2c28b4b587e658399e7c364744
Haberno <52333828+Haberno@users.noreply.github.com>
2023-07-03 03:45:48 -0400

    Add speedy kill (#75)
    * Adds speedy kill
    
    * Register speedy kill
    
    * Merge main

---
+++ src/main/kotlin/org/thepitcommunityserver/game/playerExperience/player/PlayerJoinLeaveMessages.kt
@@ -0,0 +1,16 @@
+package org.thepitcommunityserver.game.events
+
+import org.bukkit.ChatColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.thepitcommunityserver.util.Text
+
+object PlayerJoinLeaveMessages : Listener {
+    @EventHandler
+    fun onJoin(event: PlayerJoinEvent) {
+        event.joinMessage =
+            (ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "Welcome Back to The Pit Community Server! " + ChatColor.AQUA
+                    + event.player.name + ChatColor.GREEN + ", thank you for coming back ${ChatColor.RED.toString() + Text.HEART}")
+    }
+}
