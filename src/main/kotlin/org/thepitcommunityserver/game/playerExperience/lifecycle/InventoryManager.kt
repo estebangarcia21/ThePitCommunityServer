35869ee31bd9820affc161f5cc2f2419a7d2857b
Haberno <deathersmc@gmail.com>
2025-12-23 15:33:05 -0500

    update inventory management

--- src/main/kotlin/org/thepitcommunityserver/game/playerExperience/lifecycle/InventoryManager.kt
+++ src/main/kotlin/org/thepitcommunityserver/game/playerExperience/lifecycle/InventoryManager.kt
@@ -2,7 +2,6 @@ package org.thepitcommunityserver.game.playerExperience.lifecycle
 
 import org.bukkit.Material
 import org.bukkit.Sound
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -10,45 +9,9 @@ import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerPickupItemEvent
 import org.bukkit.inventory.ItemStack
-import org.thepitcommunityserver.game.enchants.lib.isEmptyItemStack
 import org.thepitcommunityserver.util.*
 
 object InventoryManager : Listener {
-    /**
-     * Returns true if the player auto-equipped the item.
-     */
-    fun autoEquip(player: Player, item: ItemStack): Boolean {
-        player.playSound(player.location, Sound.HORSE_SADDLE, 1f, 1f)
-
-        val itemType = item.type
-        val inventory = player.inventory
-
-        var equippedItem = false
-
-        if (isChestplate(itemType)) {
-            if (isEmptyItemStack(inventory.chestplate) || hasNBTEntryFor(inventory.chestplate.nbt, NBT.AUTO_EQUIP_OVERRIDABLE.key)) {
-                inventory.chestplate = item.clone()
-                equippedItem = true
-            }
-        } else if (isLeggings(itemType)) {
-            if (isEmptyItemStack(inventory.leggings) || hasNBTEntryFor(inventory.leggings.nbt, NBT.AUTO_EQUIP_OVERRIDABLE.key)) {
-                inventory.leggings = item.clone()
-                equippedItem = true
-            }
-        } else if (isBoots(itemType)) {
-            if (isEmptyItemStack(inventory.boots) || hasNBTEntryFor(inventory.boots.nbt, NBT.AUTO_EQUIP_OVERRIDABLE.key)) {
-                inventory.boots = item.clone()
-                equippedItem = true
-            }
-        } else if (isHelmet(itemType)) {
-            if (isEmptyItemStack(inventory.helmet) || hasNBTEntryFor(inventory.helmet.nbt, NBT.AUTO_EQUIP_OVERRIDABLE.key)) {
-                inventory.helmet = item.clone()
-                equippedItem = true
-            }
-        }
-
-        return equippedItem
-    }
 
     @EventHandler(priority = EventPriority.LOW)
     fun onPlayerDeath(event: PlayerDeathEvent) {
@@ -56,12 +19,22 @@ object InventoryManager : Listener {
             val inventory = event.entity.inventory
 
             val newArmorContents = arrayOfNulls<ItemStack>(4)
-            inventory.armorContents.filter { hasNBTEntryFor(it.nbt, NBT.LOSE_ON_DEATH.key) }.forEachIndexed { i, item ->
+            inventory.armorContents.filter {
+                hasNBTEntryFor(it.nbt, NBT.LOSE_ON_DEATH.key) && !hasNBTEntryFor(
+                    it.nbt,
+                    NBT.KEPT_ON_DEATH.key
+                )
+            }.forEachIndexed { i, item ->
                 newArmorContents[i] = ItemStack(Material.AIR)
             }
-            inventory.armorContents = newArmorContents
 
-            inventory.contents.filter { hasNBTEntryFor(it.nbt, NBT.LOSE_ON_DEATH.key) }.forEach {
+            inventory.armorContents = newArmorContents
+            inventory.contents.filter {
+                hasNBTEntryFor(it.nbt, NBT.LOSE_ON_DEATH.key) && !hasNBTEntryFor(
+                    it.nbt,
+                    NBT.KEPT_ON_DEATH.key
+                )
+            }.forEach {
                 inventory.remove(it)
             }
 
@@ -85,17 +58,39 @@ object InventoryManager : Listener {
 
             event.itemDrop.remove()
         }
-
-        handleRemoveOnDrop()
         handleUndroppableItems()
+        handleRemoveOnDrop()
     }
 
     @EventHandler
     fun onPickupItem(event: PlayerPickupItemEvent) {
-        val equipped = autoEquip(event.player, event.item.itemStack)
-        if (equipped) {
+        val player = event.player
+        val item = event.item.itemStack
+        val material = item.type
+
+        if (hasNBTEntryFor(item.nbt, NBT.DISABLE_PICKUP_EXISTS_IN_INVENTORY.key)) {
+            if (playerHasItem(player, material)) {
+                event.isCancelled = true
+                return
+            }
+        }
+
+        if (hasNBTEntryFor(item.nbt, NBT.AUTO_EQUIP.key) && isArmor(material)) {
+            val currentPiece = getEquippedArmorPiece(player, material)
+            val canOverride = currentPiece == null ||
+                    hasNBTEntryFor(currentPiece.nbt, NBT.AUTO_EQUIP_OVERRIDABLE.key)
+
+            if (canOverride && isArmorPieceStronger(player, material)) {
+                equipArmorPiece(player, item, currentPiece)
+                player.playSound(player.location, Sound.HORSE_ARMOR, 1f, 1f)
+            } else {
+                addItemToPlayerInventory(player, item)
+                player.playSound(player.location, Sound.ITEM_PICKUP, 1f, 1f)
+            }
+
             event.isCancelled = true
             event.item.remove()
+            return
         }
     }
 }
